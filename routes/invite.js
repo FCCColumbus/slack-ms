bdd2c9b83036b1e924d795fe370a815b5b402258
DanStockham <ds321609@gmail.com>
2017-07-16 21:07:38 -0400

    invite route connected

--- routes/invite.js
+++ routes/invite.js
@@ -1,10 +1,25 @@
 var express = require('express');
+var axios = require('axios');
 var router = express.Router();
+require('dotenv').config();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  console.log('This is our invite page');
-  res.send('This is our invite controller');
+	var slackBase = 'https://slack.com/api/users.admin.invite';
+
+	axios.get(slackBase, {
+		params: {
+			token: process.env.CLIENT_TOKEN,
+			email:'ds321609@ohio.edu',
+			first_name: 'dan'
+		}
+		
+	}).then(function(response) {
+		console.log(response.data);
+		res.json(response.data);
+	});
+
+	
 });
 
 module.exports = router;
