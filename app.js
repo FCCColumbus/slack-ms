d2c4cb5a4933cf1c2b56f12e7f3e1d98499100b7
Daniel Stockham <ds321609@gmail.com>
2018-03-04 20:59:59 -0500

    Replaced regex with strings

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ require('dotenv').config()
 const corsOptions = {
   origin: (process.NODE_ENIVORNMENT === 'development'
     ? /https?:\/\/localhost:\d+/
-    : [/\.netlify\.com$/, /https?:\/\/fcccolumbus\.com.+$/, /https?:\/\/shrouded-fortress-68159\.herokuapp.com\.com.+$/]),
-    // : ['https://www.netlify.com', 'https://fcccolumbus.com']),
+    // : [/\.netlify\.com$/, /https?:\/\/fcccolumbus\.com.+$/, /https?:\/\/shrouded-fortress-68159\.herokuapp.com\.com.+$/]),
+    : ['https://www.netlify.com', 'https://fcccolumbus.com']),
     methods: 'GET, POST',
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
