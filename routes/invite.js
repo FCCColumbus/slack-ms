d1f52835a5782c3d3d585c7cb18c3f0d7312f466
Daniel Stockham <ds321609@gmail.com>
2018-03-04 21:49:38 -0500

    Removed console logs

--- routes/invite.js
+++ routes/invite.js
@@ -3,13 +3,8 @@ const slackController = require('../controller/Slack');
 const router = express.Router();
 require('dotenv').config();
 
-const logHeaders = (req, res, next) => {
-    console.log(req.headers)
-    next();
-}
-
 //original callback for Slack's API call
-router.post('/', logHeaders, slackController.sendInvite);
+router.post('/', slackController.sendInvite);
 
 
 
